02da08b2a8efb9c089362b1724fe91b7bdb2fd5f
Svetoslav Yankov <svetoslav.yankov@nih.gov>
2017-07-18 13:51:47 -0400

    catch server error

--- src/server/server.js
+++ src/server/server.js
@@ -14,11 +14,17 @@ function setup( ssl ) {
 module.exports = {
     create: function ( logger, config, app, cb ) {
         var options = setup( config.ssl );
-        http.createServer( app ).listen( config.web.http_port, cb );
+        var server;
+        server = http.createServer( app ).listen( config.web.http_port, cb );
         logger.info( 'Accepting http requests on port ' + config.web.http_port );
         if ( options ) {
-            https.createServer( options, app ).listen( config.web.ssl_port, cb );
+            server = https.createServer( options, app ).listen( config.web.ssl_port, cb );
             logger.info( 'Accepting https requests on port ' + config.web.ssl_port );
         }
+
+        server.on('error', function(err) {
+            logger.error('Server errror: ' + err);
+        });
+
     }
 };
