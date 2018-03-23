c7b87c84c87cb5b7f12d156bbb025463719f80da
Svetoslav Yankov <svetoslav.yankov@nih.gov>
2018-03-23 11:45:26 -0400

    local VDS model changes

--- CLI/src/model/vdsModel.js
+++ CLI/src/model/vdsModel.js
@@ -19,6 +19,28 @@ const reloadUsers = async () => {
 
     try {
         const users = await vdsConnector.getUsers(null, 'nci');
+
+        users.forEach(user => {
+            user.NEDId = user.UNIQUEIDENTIFIER;
+            user.FirstName = user.GIVENNAME;
+            user.MiddleName = user.MIDDLENAME;
+            user.LastName = user.NIHMIXCASESN;
+            user.Email = getEmail(user);
+            user.Phone = user.TELEPHONENUMBER;
+            user.Classification = user.ORGANIZATIONALSTAT;
+            user.SAC = user.NIHSAC;
+            user.AdministrativeOfficerId = user.NIHSERVAO;
+            user.COTRId = user.NIHCOTRID;
+            user.ManagerId = user.MANAGER;
+            user.Locality - user.L;
+            user.PointOfContactId = user.NIHPOC;
+            user.Division = getDivision(user);
+            user.Locality = user.L;
+            user.Site = user.NIHSITE;
+            user.Building = getBuilding(user);
+            user.Room = user.ROOMNUMBER;
+        });
+
         await collection.insertMany(users, {
             ordered: false
         });
@@ -51,13 +73,31 @@ const updateUsers = async () => {
         const ops = [];
 
         users.forEach(user => {
+            user.NEDId = user.UNIQUEIDENTIFIER;
+            user.FirstName = user.GIVENNAME;
+            user.MiddleName = user.MIDDLENAME;
+            user.LastName = user.NIHMIXCASESN;
+            user.Email = getEmail(user);
+            user.Phone = user.TELEPHONENUMBER;
+            user.Classification = user.ORGANIZATIONALSTAT;
+            user.SAC = user.NIHSAC;
+            user.AdministrativeOfficerId = user.NIHSERVAO;
+            user.COTRId = user.NIHCOTRID;
+            user.ManagerId = user.MANAGER;
+            user.Locality - user.L;
+            user.PointOfContactId = user.NIHPOC;
+            user.Division = getDivision(user);
+            user.Locality = user.L;
+            user.Site = user.NIHSITE;
+            user.Building = getBuilding(user);
+            user.Room = user.ROOMNUMBER;
             ops.push({
                 replaceOne:
-                {
-                    filter: { UNIQUEIDENTIFIER: user.UNIQUEIDENTIFIER },
-                    replacement: user,
-                    upsert: true
-                }
+                    {
+                        filter: { UNIQUEIDENTIFIER: user.UNIQUEIDENTIFIER },
+                        replacement: user,
+                        upsert: true
+                    }
             });
         });
         await collection.bulkWrite(ops);
@@ -69,5 +109,61 @@ const updateUsers = async () => {
         process.exit(1);
     }
 };
+
+const getEmail = (obj) => {
+
+    let result = null;
+
+    const proxyEmails = obj.proxyAddresses;
+    if (proxyEmails) {
+        if (Array.isArray(proxyEmails)) {
+            proxyEmails.forEach(email => {
+                const data = email.split(':');
+                if (data[0] === 'SMTP') {
+                    result = data[1];
+                }
+            });
+        } else {
+            const data = proxyEmails.split(':');
+            if (data[0] === 'SMTP') {
+                result = data[1];
+            }
+        }
+    }
+    return result;
+};
+
+const getBuilding = (obj) => {
+
+    if (obj.BUILDINGNAME) {
+        return 'BG ' + obj.BUILDINGNAME;
+    } else {
+        return 'N/A';
+    }
+};
+
+const getDivision = (obj) => {
+
+    let result = 'N/A';
+
+    if (obj.NIHORGPATH) {
+        const orgPathArr = obj.NIHORGPATH.split(' ') || [];
+        const len = orgPathArr.length;
+
+        if (len > 0 && len <= 2) {
+            result = orgPathArr[len - 1];
+        } else if (len > 2) {
+            if (orgPathArr[1] === 'OD') {
+                result = orgPathArr[2];
+            } else {
+                result = orgPathArr[1];
+            }
+        }
+    }
+
+    return result;
+
+};
+
 
 module.exports = { updateUsers, reloadUsers };
