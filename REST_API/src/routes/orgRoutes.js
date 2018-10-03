f9d9ac388bc2d2e6123d501cd07e7c93fb589d9b
Ming Ying <yingming18@gmail.com>
2018-10-03 10:09:57 -0400

    Add search for subbranch by SAC and ShortName APIs.

--- REST_API/src/routes/orgRoutes.js
+++ REST_API/src/routes/orgRoutes.js
@@ -24,10 +24,47 @@ const router = () => {
     apiRouter.route('/short-name/:shortName')
         .get(async function (req, res) {
             try {
-                logger.info('short-name');
                 const org = await getOrgByFilter({shortName: req.params.shortName.toUpperCase()});
                 if (org) {
                     res.json(org);
+                } else {
+                    res.status(400).send('Organization not found');
+                }
+            } catch (error) {
+                res.status(500).send(error);
+            }
+        });
+
+    apiRouter.route('/subbranch/sac/:sac')
+        .get(async function (req, res) {
+            try {
+                const org = await getOrgByFilter({parentSac: req.params.sac.toUpperCase()});
+                if (org) {
+                    res.json(org);
+                } else {
+                    res.status(400).send('Organization not found');
+                }
+            } catch (error) {
+                res.status(500).send(error);
+            }
+        });
+
+    apiRouter.route('/subbranch/short-name/:shortName')
+        .get(async function (req, res) {
+            try {
+                const orgs = await getOrgByFilter({shortName: req.params.shortName.toUpperCase()});
+                if (orgs) {
+                    const results = [];
+                    if (orgs.length > 0) {
+                        for ( const org of orgs) {
+                            const sac = org['sac'];
+                            const subOrgs = await getOrgByFilter({parentSac: sac});
+                            if (subOrgs.length > 0) {
+                                results.push(subOrgs);
+                            }
+                        }
+                    }
+                    res.json(results);
                 } else {
                     res.status(400).send('Organization not found');
                 }
