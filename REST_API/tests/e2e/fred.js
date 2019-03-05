a863dec8a60ab0624f6cae32f87fc654a7aa274c
Ming Ying <yingming18@gmail.com>
2019-03-05 17:35:43 -0500

    Added end to end testing for Fredric APIs.

---
+++ REST_API/tests/e2e/fred.js
@@ -0,0 +1,137 @@
+const request = require('request');
+const { expect } = require('chai');
+const { config } = require('../../constants');
+
+describe('Tesing Fredric APIs', function() {
+    const address = process.env['TEST_API'];
+    const userName = config.users[0].user;
+    const password = config.users[0].password;
+    const pageSize = 50;
+    const OK = 200;
+    const defaultPageSize = 1000;
+    const minPropertyCount = 1000;
+    const timeOut = 10 * 1000; // 10 seconds
+    const officerId = process.env['OFFICER_ID'] || '0011231141';
+    const custodianId = process.env['CUSTODIAN_ID'] || '0014049524';
+
+    it('Should be able to get properties (default page size)', function(done) {
+        this.timeout(timeOut); // 10 seconds
+        request(`${address}/api/fred/props/`,
+            {
+                'auth': {
+                    'username': userName,
+                    'password': password
+                },
+                'headers': {
+                    'Accept': 'application/json'
+                }
+            },
+            function (error, response, body) {
+                expect(error).to.be.a('null');
+                expect(response).not.to.be.a('null');
+                expect(response.statusCode).to.equal(OK);
+                const results = JSON.parse(body);
+                expect(results).not.to.be.a('null');
+                expect(results.length).to.at.most(defaultPageSize);
+                done();
+            });
+    });
+
+    it('Should be able to get properties specify page size to be 50', function(done) {
+        this.timeout(timeOut); // 10 seconds
+        request(`${address}/api/fred/props/?pageSize=${pageSize}`,
+            {
+                'auth': {
+                    'username': userName,
+                    'password': password
+                },
+                'headers': {
+                    'Accept': 'application/json'
+                }
+            },
+            function (error, response, body) {
+                expect(error).to.be.a('null');
+                expect(response).not.to.be.a('null');
+                expect(response.statusCode).to.equal(OK);
+                const results = JSON.parse(body);
+                expect(results).not.to.be.a('null');
+                expect(results.length).to.at.most(pageSize);
+                done();
+            });
+    });
+
+    it('Should be able to get property total count', function(done) {
+        this.timeout(timeOut); // 10 seconds
+        request(`${address}/api/fred/props/count`,
+            {
+                'auth': {
+                    'username': userName,
+                    'password': password
+                },
+                'headers': {
+                    'Accept': 'application/json'
+                }
+            },
+            function (error, response, body) {
+                expect(error).to.be.a('null');
+                expect(response).not.to.be.a('null');
+                expect(response.statusCode).to.equal(OK);
+                const results = JSON.parse(body);
+                expect(results).not.to.be.a('null');
+                expect(results.count).to.above(minPropertyCount);
+                done();
+            });
+    });
+
+    it('Should be able to getting Fred properties for property officer', function(done) {
+        this.timeout(timeOut); // 10 seconds
+        request(`${address}/api/fred/props/officer/${ officerId }`,
+            {
+                'auth': {
+                    'username': userName,
+                    'password': password
+                },
+                'headers': {
+                    'Accept': 'application/json'
+                }
+            },
+            function (error, response, body) {
+                expect(error).to.be.a('null');
+                expect(response).not.to.be.a('null');
+                expect(response.statusCode).to.equal(OK);
+                const results = JSON.parse(body);
+                expect(results).not.to.be.a('null');
+                expect(results.length).to.above(1);
+                for (const prop of results) {
+                    expect(prop.PropertyOfficerNedId).to.equal(officerId);
+                }
+                done();
+            });
+    });
+
+    it('Should be able to getting Fred properties for custodian', function(done) {
+        this.timeout(timeOut); // 10 seconds
+        request(`${address}/api/fred/props/custodian/${ custodianId }`,
+            {
+                'auth': {
+                    'username': userName,
+                    'password': password
+                },
+                'headers': {
+                    'Accept': 'application/json'
+                }
+            },
+            function (error, response, body) {
+                expect(error).to.be.a('null');
+                expect(response).not.to.be.a('null');
+                expect(response.statusCode).to.equal(OK);
+                const results = JSON.parse(body);
+                expect(results).not.to.be.a('null');
+                expect(results.length).to.above(1);
+                for (const prop of results) {
+                    expect(prop.CustodianNedId).to.equal(custodianId);
+                }
+                done();
+            });
+    });
+});
